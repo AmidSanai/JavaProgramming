967d0dee485c13251f9670957dffd636011baf87
Amid Sanai <amidsanai9@gmail.com>
2020-11-01 12:53:27 +0330

    =Practice of Map in java

--- src/sesson15/PracticeOfCollectionSample.java
+++ src/sesson15/PracticeOfCollectionSample.java
@@ -1,4 +1,16 @@
 package sesson15;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PracticeOfCollectionSample {
+    public static void main(String[] args) {
+        Map<String, Integer> family = new HashMap<>();
+        family.put("Youngest children", 13);
+        family.put("Oldest children", 18);
+        family.put("Mom", 44);
+        family.put("Dad", 47);
+        //Printing
+        System.out.println(family);
+    }
 }
