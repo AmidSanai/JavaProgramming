10d406927a21adce96be18ee3f91f110605b1149
Amid Sanai <amidsanai9@gmail.com>
2020-11-02 21:52:55 +0330

    =Practice for List

---
+++ src/sesson15/PracticeOfArrayList.java
@@ -0,0 +1,25 @@
+package sesson15;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PracticeOfArrayList {
+
+    public static void main(String[] args) {
+        //Out list
+        List<String> colors = new ArrayList<>();
+        colors.add("Red");
+        colors.add("Black");
+        colors.add("yellow");
+        colors.add("Green");
+        colors.add("Blue");
+        //Print them out
+        int i = 1;
+        int i1 = 0;
+        for (String c : colors) {
+            System.out.print(i++);
+            System.out.print("-");
+            System.out.println(colors.get(i1++));
+        }
+    }
+}
